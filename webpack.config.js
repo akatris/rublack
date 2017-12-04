07bad57b50fbf8655468f498a740376e8e4045af
Sitraka Ratsimba <ratsimbasitraka@akatris.com>
2017-12-04 21:16:46 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+    entry: './source/_index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'source')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.(woff|svg|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+
+        ]
+    }
+};
